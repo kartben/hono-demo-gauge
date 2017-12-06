49745be267854bd99fb2f1fd1c8f29dbd93a088d
Benjamin Cabé <benjamin.cabe@eclipse-foundation.org>
2017-12-06 12:07:11 -0600

    -

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as Influx from 'influx';
 
 const app = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var influxdbhost = process.env.INFLUXDB_PORT_8086_TCP_ADDR || 'influxdb-hono.192.168.64.7.nip.io',
